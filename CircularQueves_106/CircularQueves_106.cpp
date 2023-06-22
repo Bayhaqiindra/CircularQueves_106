5c25ac4c905de9d7e3ecd517410dbba98ee9f61d
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-06-22 15:23:19 +0700

    pengecekan apakah queue kosong atau tidak dengan memeriksa apakah FRONT sama dengan -1. Jika kondisi tersebut terpenuhi, berarti queue kosong dan tidak ada elemen yang dapat dihapus. Pada kondisi ini, pesan "Queve Underflow" akan ditampilkan menggunakan cout untuk menandakan bahwa operasi penghapusan tidak dapat dilakukan. Setelah itu, fungsi remove() akan mengembalikan kendali ke pemanggil. Jika queue tidak kosong, maka elemen di depan antrian (indeks FRONT) dapat dihapus. Pada bagian ini, elemen yang dihapus ditampilkan menggunakan cout dengan pesan "The element deleted from the queve is: " diikuti dengan nilai dari queve_array[FRONT]. Ini menunjukkan elemen mana yang dihapus dari queue.

--- CircularQueves_106/CircularQueves_106.cpp
+++ CircularQueves_106/CircularQueves_106.cpp
@@ -39,4 +39,10 @@ public:
 }
 
 	void remove() {
-	
+		//Cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << " Queve Underflow\n";
+			return;
+		}
+		cout << "\nThe element deleted from the queve is: " << queve_array[FRONT] << "\M";
+

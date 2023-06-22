44f695b4b442352021fe72402811f2f2c6ce4df9
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-06-22 15:54:31 +0700

    Jika FRONT_position lebih kecil dari REAR_position, ini menandakan bahwa elemen-elemen dalam queue berada dalam rentang yang tidak melintasi batas akhir array. Di dalam blok while, dilakukan perulangan dari FRONT_position hingga REAR_position. Dalam setiap iterasi perulangan, elemen pada indeks FRONT_position dari array queve_array akan ditampilkan menggunakan cout. Setelah itu, FRONT_position akan ditambah 1 untuk maju ke indeks berikutnya dalam array. Perulangan akan terus berlanjut hingga mencapai atau melebihi REAR_position. Setelah perulangan selesai, akan dilakukan penampilan baris baru dengan cout << endl;. Ini digunakan untuk memisahkan hasil tampilan dari elemen-elemen dalam queue dengan konten berikutnya yang akan ditampilkan (jika ada).

--- CircularQueves_106/CircularQueves_106.cpp
+++ CircularQueves_106/CircularQueves_106.cpp
@@ -88,3 +88,14 @@ public:
 			}
 
 			FRONT_position = 0;
+
+			// literasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queve_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+	}
+};
+
